f6e0cf0ac6df5f54dd40f9c8212deb7f88f87503
Kamran Grasselli <KGrasselli@Soliel.local>
2018-03-10 12:29:33 -0500

    Just fixing broken code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,31 @@
 
 // Set up variables
-var userText = document.getElementById("user-text");
-var compText = document.getElementById("comp-text");
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-
-var wins = 0;
-var losses = 0;
+var userText;
+var compText;
 var guessesLeft = 9;
+
+var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+var compSelect = possible.charAt(Math.floor(Math.random() * possible.length));
+console.log("compSelect:  " + compSelect);
 
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function(keyListen) {
-    userText.textContent = keyListen.key;
-    console.log(userText);
-    
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    var compSelect = possible.charAt(Math.floor(Math.random() * possible.length));
-    console.log(compSelect);
-
-
-    // Pass random selection to variable
-    compText.textContent = compSelect;
-    console.log(compText);
+    userText = keyListen.key;
+    console.log("userText:  " + userText);
+    document.getElementById("user-text").innerHTML = userText;
     
 
     // Allow 9 guesses and calculate wins and losses
     for (i = 0; i < 10; i++) {    
         if (userText === compSelect) {
-            wins++;
+            document.getElementById("bingo").innerHTML = ("BINGO!!<p>The computer chose:  " + compSelect);
+            return false;
+
         } else {
-            losses++;
             guessesLeft--;
         }
     }
 
-    winsText.textContent = wins;
-    lossesText.textContent = losses;
-
-}
+};
 
