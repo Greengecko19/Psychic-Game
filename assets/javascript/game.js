0ab39d6a9c28d4776626cb7ac2632cac5973e154
Kamran Grasselli <KGrasselli@Soliel.local>
2018-03-12 12:47:35 -0400

    more fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,27 +9,36 @@ var compSelect = possible.charAt(Math.floor(Math.random() * possible.length));
 console.log("compSelect:  " + compSelect);
 
 
-// This function is run whenever the user presses a key.
-document.onkeyup = function(keyListen) {
-    userInput = keyListen.key;
-    console.log("userInput:  " + userChoices);
-    guessesLeft--;
-    userChoices.push(userInput);
-    document.getElementById("user-text").innerHTML = userChoices;
-    document.getElementById("guesses-left").innerHTML = guessesLeft;
+function main() {
 
-    if (parseInt(event.keyCode,10) == 13) {
-        location.reload(true);
-    }
-    else {
-        // Allow 9 guesses and calculate wins and losses
-        for (i = 0; i < 10; i++) {    
-            if (userInput == compSelect) {
-                document.getElementById("bingo").innerHTML = ("BINGO!!\n\nYou got it right!!");
-                return false;
+    // activated by any key press
+    document.onkeyup = function(keyListen) {
+        userInput = keyListen.key;
+        console.log("userInput:  " + userChoices);
+        guessesLeft--;
+        userChoices.push(userInput);
+        document.getElementById("user-text").innerHTML = userChoices;
+        document.getElementById("guesses-left").innerHTML = guessesLeft;
+
+        // if match, celebrate and stop script
+        if (userInput == compSelect) {
+            document.getElementById("bingo").innerHTML = ("BINGO!!\n\nYou got it right!!");
+            
+            if (guessesLeft == 9) {
+                console.log(guessesLeft);
+                userChoices = [];
             }
+        }
+
+        // press Enter to restart game
+        if (parseInt(event.keyCode,10) == 13) {
+            location.reload(true);
+            guessesLeft = 9;
+            userChoices = [];
+            return true;
         }
     }
 
-};
+}
 
+main();
